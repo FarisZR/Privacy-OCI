2ddfaae56fb2b02acef5c602c812d4c8b7b900ee
FarisZR <35614734+FarisZR@users.noreply.github.com>
2022-08-05 03:11:17 +0300

    Adapt file to GitLab

--- remote-check.sh
+++ remote-check.sh
@@ -5,8 +5,9 @@ git ls-remote $1 HEAD > $2
 local="$2"
 remote="$3"
 # if the files match, then it means the build is up-to-date, so no need to rebuild.
+# export variable to files, so they can be passed to other jobs, https://docs.gitlab.com/ee/ci/variables/#pass-an-environment-variable-to-another-job
 if cmp -s "$local" "$remote"; then
-    echo "::set-output name=$4::false"
+    echo "$4=false" >>./check.env
 else
-    echo "::set-output name=$4::true"
+    echo "$4=true" >> ./check.env
 fi
